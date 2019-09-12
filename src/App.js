a025f2d2145a3d162433b5b2194ae247e4aee4a3
joshhrycek <joshhrycek@gmail.com>
2019-09-12 17:24:01 -0400

    conditional state rendering

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 // import TheDate from './state/TheDate';
 // import Counter from './state/Counter';
 // import HelloWorld from './state-drills/HelloWorld';
-//  import Bomb from './state-drills/Bomb';
-import Roulette from './state-drills/Roulette'
+// import Bomb from './state-drills/Bomb';
+// import Roulette from './state-drills/Roulette'
+// import Tabs from './state/Tabs'
+// import tabsProp from './Store'
+import Accord from './state-drills/Accord'
+import sections from './Store'
 
 function App() {
   return (
@@ -12,7 +16,9 @@ function App() {
       {/* <Counter count= {123}/> */}
       {/* <HelloWorld /> */}
       {/* <Bomb /> */}
-      <Roulette />
+      {/* <Roulette /> */}
+      {/* <Tabs tabs= {tabsProp} prop1={"fuck you"}/> */}
+      <Accord sections={sections}/>
     </main>
   );
 }
